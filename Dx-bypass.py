3fe3e08b5a8b845ebf83c17d4bdee74e2e9bf4dc
DARK BOY <114945824+playboy167@users.noreply.github.com>
2022-10-19 20:50:12 +0600

    Create Dx-bypass.py

---
+++ Dx-bypass.py
@@ -0,0 +1,28 @@
+import os, platform,time
+print("\t NOTE : 1ST COPY KEY THEN SENT ME KEY")
+os.system('xdg-open https://www.facebook.com/100072883587040');time.sleep(1.2)
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from max import check_my_key
+ 
+        check_my_key()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        print("\t [x] SRY BRO BYPASS NOY SUPPORT")
+ 
